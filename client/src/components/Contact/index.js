e3edefd44ad96c63c1db1ea10131d58f1894f7d9
timmorse13 <timmorse13@gmail.com>
2021-05-02 23:48:53 -0700

    react

---
+++ client/src/components/Contact/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.css'
+
+
+function Contact() {
+    return (
+        <div className="Two">
+            <a name="contact-info"><h2>Contact Info</h2></a>
+            <br />
+            <p>Phone Number: (661) 645-1607</p>
+            <p><a href="mailto: timmorse13@gmail.com" target="_blank">Timmorse13@gmail.com</a></p>
+            <br />
+            <br />
+            <p><a href="https://www.linkedin.com/in/tim-morse-57158519a/" target="_blank"><button>LinkedIn Profile</button></a></p>
+        </div>
+    )
+}
+
+export default Contact
